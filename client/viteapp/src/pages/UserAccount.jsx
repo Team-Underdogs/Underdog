c6435616cae26f9be85d3d5dff35623b6865fb9f
CiarDaire <SakokuEdicts26>
2023-11-06 17:15:43 +1300

    usestate account

--- client/viteapp/src/pages/UserAccount.jsx
+++ client/viteapp/src/pages/UserAccount.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
 const UserAccount = () => {
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams();
+    
     return (
         <div>
             <h1>Public User Account</h1>
