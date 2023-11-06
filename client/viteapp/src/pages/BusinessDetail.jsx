3206edf927ea2a4e7e20ed7729c7b12c3c0ccfac
CiarDaire <SakokuEdicts26>
2023-11-06 17:13:38 +1300

    usestates on detail pages added

--- client/viteapp/src/pages/BusinessDetail.jsx
+++ client/viteapp/src/pages/BusinessDetail.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const BusinessDetail = () => {
+    const [business, setBusiness] = useState({});
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams();
+
     return (
         <div>
             <h1>Single Business Page</h1>
--- client/viteapp/src/pages/ServiceDetail.jsx
+++ client/viteapp/src/pages/ServiceDetail.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const ServiceDetail = () => {
+    const [service, setService] = useState({});
+    const [loading, setLoading] = useState(false);
+    const { id } = useParams();
+
     return (
         <div>
             <h1>Single Service Page</h1>
