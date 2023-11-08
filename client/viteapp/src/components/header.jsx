7caa32ed109510fdb2aa7b7061b3fb61c9741d17
CiarDaire <SakokuEdicts26>
2023-11-09 03:12:42 +1300

    header

--- client/viteapp/src/components/header.jsx
+++ client/viteapp/src/components/header.jsx
@@ -1,9 +1,56 @@
+import { FaShoppingCart, FaUser, FaSearch } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import underdogLogo from '../assets/underdog.png';
+import { useState } from 'react';
 
+const Header = ({ enterSearch, pickOption }) => {
+    const [searchField, setSearchField] = useState('');
+    const [selectedOption, setSelectedOption] = useState('');
 
-const Header = () => {
+    const handleSearch = () => {
+        enterSearch(searchField, selectedOption);
+    };
+
+    const handleSearchOptionChange = () => {
+        pickOption(selectedOption);
+    }
+
     return (
-        <div className="header-banner">
-            
+        <div className="header">
+            <img src={underdogLogo} alt="Underdog Logo" />
+            <div className="searchbar">
+            <div className="search-dropdown">
+                    <select 
+                        className="dropbtn"
+                        value={selectedOption}
+                        onChange={handleSearchOptionChange}
+                    >
+                        <option value="all">All</option>
+                        <option value="businesses">Businesses</option>
+                        <option value="products">Products</option>
+                        <option value="services">Services</option>
+                    </select>
+                </div>
+                <input 
+                    className="search-input"
+                    type="text"
+                    value={searchField}
+                    onChange={(e)=>setSearchField(e.target.value)}
+                    placeholder="Search businesses, products, services..."
+                />
+                <button onClick={handleSearch} className="search-button">
+                    <FaSearch size={20} />
+                </button>
+            </div>
+            <div className="header-buttons">
+                <a href="/">Sign in</a>
+                <Link to='/cart/:id'>
+                    <FaShoppingCart className="icon" size={30} />
+                </Link>
+                <Link to='/account/:id'> 
+                    <FaUser className="icon" size={30} />
+                </Link>
+            </div>
         </div>
     )
 }
--- client/viteapp/src/main.jsx
+++ client/viteapp/src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import Header from './components/header.jsx';
 import { BrowserRouter } from "react-router-dom";
+import Background from './components/gradientbackground.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Header />
+    <Background>
       <App />
+    </Background>
   </BrowserRouter>
 );
