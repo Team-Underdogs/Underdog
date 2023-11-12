cef0ceb9f94cc511ec162ec4c6a9b037e006178d
CiarDaire <SakokuEdicts26>
2023-11-13 02:35:34 +1300

    browse cards

--- client/viteapp/src/pages/Home.jsx
+++ client/viteapp/src/pages/Home.jsx
@@ -1,12 +1,24 @@
+import BrowseCards from "../components/BrowseCards";
+
 const Home = () => {
     return (
-        <div>
-            <div className="browse-container">
-                <div className="browse-text">
-                    <h1>Shop With Purpose</h1>
-                    <h4>Start browsing for products or businesses that embody the values you want to support.</h4>
-                </div>
+        <div className="browse-container">
+            <div className="browse-text">
+                <h1>Shop With Purpose</h1>
+                <h4>Start browsing for products or businesses that embody the values you want to support.</h4>
             </div>
+            <div className="browse-options">
+                <BrowseCards
+                    image='../src/assets/business.jpeg'
+                    title='Businesses'
+                    link='/browse/business'
+                />
+                <BrowseCards
+                     image='../src/assets/products.jpg'
+                    title='Products & Services'
+                    link='/browse/product'
+                />
+            </div> 
         </div>
     );
 }
