f13d97f605fc18f6c49598c0ef5b881b73804e62
CiarDaire <SakokuEdicts26>
2023-11-18 18:19:55 +1300

    id links

--- client/viteapp/src/components/ProductCard.jsx
+++ client/viteapp/src/components/ProductCard.jsx
@@ -1,16 +1,12 @@
-import { Link } from 'react-router-dom';
-
 const ProductCard = ({ productID, productName, businessName, price, image }) => {
     return (
         <div className="productcard-container">
-            <Link to={productID}>
-                <img src={image} />
-                <div className="productcard-textbanner">
-                    <p className="productcard-title">{productName}</p>
-                    <p className="productcard-business">{businessName}</p>
-                </div>
-                <p className="productcard-price">{price}</p>
-            </Link>
+            <img src={image} />
+            <div className="productcard-textbanner">
+                <p className="productcard-title">{productName}</p>
+                <p className="productcard-business">{businessName}</p>
+            </div>
+            <p className="productcard-price">{price}</p>
         </div>
     )
 }
